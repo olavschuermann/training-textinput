b8f34670a21f7b53a060ee1ef6fb4cf7f1c624fe
Olav Schuermann <developer@netzpluslogik.de>
2020-02-15 17:17:29 +0100

    setting hook for input data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // Eingabe in Input-Feld auslesen + in Funktion schreiben
+  const [inputData, setInputData] = useState("");
+
+
   return (
     <div className="App">
       <label>Ihre Eingabe:</label> <br />
-      <input type="text"></input>
+      <input type="text" onChange={}></input>
       <br /> <br />
       Maximale Länge:
       <br /><br />
