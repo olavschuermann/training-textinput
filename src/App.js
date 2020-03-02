f1a928c3628e2447ae5ea492ff4a9e41e32e87c6
Olav Schuermann <developer@netzpluslogik.de>
2020-03-02 17:34:22 +0100

    using hooks for text input

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 
 // Goal of app:
-// Texteingabe-Feld
+// Texteingabe-Feld + Button für Absenden
 // Text wird per Alert ausgegeben
+// Hooks verwenden
 // Mindestlänge des Textes wird überprüft (1)
 // Maximallänge des Textes wird überprüft (2)
 // Bei Verstößen gegen Minimal- oder Maximallänge wird Absenden-Button deaktivier
 
 
 function App() {
-  return (
+
+// Eingabe aus Textfeld    
+const [textInput, setTextInput] = useState("");
+   
+return (
     <div className="App">
         <form>
             <label>Bitte geben Sie einen Text ein:</label>
